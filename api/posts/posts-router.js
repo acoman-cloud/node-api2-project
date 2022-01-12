8cc6c9dcd13ff8959143fccaf1f72cf5ceb152a4
Anthony Coman <acoman@yahoo.com>
2022-01-11 20:37:24 -0600

    cannot get any info, also random errors

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 const Post = require('./posts-model.js')
 
-router.get('/api/posts/', (req, res) => {
+router.get('/', (req, res) => {
   Post.find(req.query)
     .then(posts => {
       res.status(200).json(posts)
@@ -16,7 +16,7 @@ router.get('/api/posts/', (req, res) => {
     })
 })
 
-router.get('/api/posts/:id', (req, res) => {
+router.get('/:id', (req, res) => {
   Post.findById(req.params.id)
     .then(posts => {
       if (posts) {
@@ -33,7 +33,7 @@ router.get('/api/posts/:id', (req, res) => {
     })
 })
 
-router.post('/api/posts/', (req, res) => {
+router.post('/', (req, res) => {
   Post.insert(req.body)
     .then(posts => {
       res.status(201).json(posts)
@@ -46,29 +46,29 @@ router.post('/api/posts/', (req, res) => {
     })
 })
 
-router.delete('/api/posts/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
   Post.remove(req.params.id)
     .then(count => {
       if (count > 0) {
         res.status(200).json({ message: 'The post has been nuked' })
       } else {
-        res.status(404).json({ message: 'The adopter could not be found' })
+        res.status(404).json({ message: 'The post could not be found' })
       }
     })
     .catch(error => {
       console.log(error)
       res.status(500).json({
-        message: 'Error removing the adopter',
+        message: 'Error removing the post',
       })
     })
 })
 
-router.put('/api/posts/:id', (req, res) => {
+router.put('/:id', (req, res) => {
   const changes = req.body
   Post.update(req.params.id, changes)
-    .then(adopter => {
-      if (adopter) {
-        res.status(200).json(adopter)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post)
       } else {
         res.status(404).json({ message: 'The post with the specified ID does not exist' })
       }
@@ -81,7 +81,7 @@ router.put('/api/posts/:id', (req, res) => {
     })
 })
 
-router.get('/api/posts/:id/comments', (req, res) => {
+router.get('/:id/comments', (req, res) => {
   Post.find(req.query)
     .then(posts => {
       res.status(200).json(posts)
--- api/server.js
+++ api/server.js
@@ -1,14 +1,19 @@
 // implement your server here
 // require your posts router and connect it here
 const express = require('express')
-//const postRouter = require('./posts/posts-router.js')
+const postRouter = require('./posts/posts-router.js')
 
 const server = express()
 
 server.use(express.json())
 
-//server.use('/api/posts', postRouter)
+server.use('/api/posts', postRouter)
 
-
+server.get('/', (req, res) => {
+  res.send(`
+    <h2>Shelter API</h>
+    <p>Welcome to the Shelter API</p>
+  `)
+})
 
 module.exports = server
