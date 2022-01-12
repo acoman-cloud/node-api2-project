8f914513e3ab9b927bd3e10b314046a16a8e5c0b
Anthony Coman <acoman@yahoo.com>
2022-01-11 19:50:06 -0600

    I got the server to run

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,97 @@
 // implement your posts router here
+const express = require('express')
+const Post = require('./posts-model.js')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  Post.find(req.query)
+    .then(posts => {
+      res.status(200).json(posts)
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json({
+        message: 'The posts information could not be retrieved',
+      })
+    })
+})
+
+router.get('/:id', (req, res) => {
+  Post.findById(req.params.id)
+    .then(posts => {
+      if (posts) {
+        res.status(200).json(Post)
+      } else {
+        res.status(404).json({ message: 'The post with the specified ID does not exist' })
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json({
+        message: 'The post information could not be retrieved',
+      })
+    })
+})
+
+router.post('/', (req, res) => {
+  Post.add(req.body)
+    .then(posts => {
+      res.status(201).json(posts)
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json({
+        message: 'The post could not be created',
+      })
+    })
+})
+
+router.delete('/:id', (req, res) => {
+  Post.remove(req.params.id)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({ message: 'The post has been nuked' })
+      } else {
+        res.status(404).json({ message: 'The adopter could not be found' })
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json({
+        message: 'Error removing the adopter',
+      })
+    })
+})
+
+router.put('/:id', (req, res) => {
+  const changes = req.body
+  Post.update(req.params.id, changes)
+    .then(adopter => {
+      if (adopter) {
+        res.status(200).json(adopter)
+      } else {
+        res.status(404).json({ message: 'The post with the specified ID does not exist' })
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json({
+        message: 'Error updating the adopter',
+      })
+    })
+})
+
+router.get('/', (req, res) => {
+  Post.find(req.query)
+    .then(posts => {
+      res.status(200).json(posts)
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json({
+        message: 'The posts information could not be retrieved',
+      })
+    })
+})
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,2 +1,15 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express')
+
+const server = express()
+
+server.use(express.json())
+
+server.get('/', (req, res) => {
+  res.send(`
+    <h2>Welcome to the jungle</h>
+  `)
+})
+
+module.exports = server
